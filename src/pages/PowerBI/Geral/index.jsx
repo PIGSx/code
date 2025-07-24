15a319ec1702f32b79f51e0ab270b3b0858ac964
PIGSx <hiury1315ajac@gmail.com>
2025-07-24 08:22:46 -0300

    style

--- src/pages/PowerBI/Geral/index.jsx
+++ src/pages/PowerBI/Geral/index.jsx
@@ -1,11 +1,11 @@
 const Geral = () => {
   return (
-    <div>
+    <div className="geral">
       <h1>Dashboard - Geral</h1>
       <iframe
         title="J E H"
-        width="1140"
-        height="541.25"
+        width="1700"
+        height="900"
         src="https://app.powerbi.com/reportEmbed?reportId=274328ad-f3ed-473b-8161-57e71fedecb9&autoAuth=true&ctid=38ae2f02-5710-4e12-80bb-83600c3fdf1e"
         frameborder="0"
         allowFullScreen="true"
