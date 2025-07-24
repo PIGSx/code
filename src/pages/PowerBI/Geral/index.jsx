1f0cdb2f985cf55df797a801f8c54c6d9ba6c6ca
PIGSx <hiury1315ajac@gmail.com>
2025-07-24 07:39:09 -0300

    teste

---
+++ src/pages/PowerBI/Geral/index.jsx
@@ -0,0 +1,16 @@
+const Geral = () => {
+  return (
+    <div>
+      <h1>Dashboard - Geral</h1>
+      <iframe
+        title="J E H"
+        width="1140"
+        height="541.25"
+        src="https://app.powerbi.com/reportEmbed?reportId=274328ad-f3ed-473b-8161-57e71fedecb9&autoAuth=true&ctid=38ae2f02-5710-4e12-80bb-83600c3fdf1e"
+        frameborder="0"
+        allowFullScreen="true"
+      ></iframe>
+    </div>
+  );
+};
+export default Geral;
