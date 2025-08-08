6f31bba71e69f7555f0cd70b7a1d4ba09284642f
PIGSx <hiury1315ajac@gmail.com>
2025-08-08 11:26:35 -0300

    teste pyhton

--- cobra/app.py
+++ cobra/app.py
@@ -13,22 +13,28 @@ CORS(app)  # Permite conexão do React
 @app.route('/abrir-site', methods=['POST'])
 def abrir_site():
     try:
-        # Configurações do Chrome
+        # Configurações do Chrome (modo headless para Render)
         options = webdriver.ChromeOptions()
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--headless')  # sem interface
+        options.add_argument('--disable-gpu')
+        options.add_argument('--window-size=1920x1080')
 
         # Inicia navegador
         navegador = webdriver.Chrome(options=options)
-        navegador.maximize_window()
 
         # Abre o site e faz login
         navegador.get("https://web.hapolo.com.br/")
-        navegador.find_element(By.XPATH, '/html/body/div/div/div/div/section/form[1]/div/div[1]/input').send_keys("psbltda")
-        navegador.find_element(By.XPATH, '/html/body/div/div/div/div/section/form[1]/div/div[2]/input').send_keys("010203" + Keys.RETURN)
+        navegador.find_element(
+            By.XPATH, '/html/body/div/div/div/div/section/form[1]/div/div[1]/input'
+        ).send_keys("psbltda")
+        navegador.find_element(
+            By.XPATH, '/html/body/div/div/div/div/section/form[1]/div/div[2]/input'
+        ).send_keys("010203" + Keys.RETURN)
 
-        # Mantém a janela aberta 
-        time.sleep(5000)
+        # Mantém a janela aberta por um tempo (só para teste)
+        time.sleep(5)
 
         return jsonify({"status": "sucesso", "mensagem": "Navegador aberto com login realizado."})
 
@@ -36,4 +42,4 @@ def abrir_site():
         return jsonify({"status": "erro", "mensagem": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
